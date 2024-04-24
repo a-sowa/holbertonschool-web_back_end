6be4fe1afe8ddaa98cb28a61524fa4aeaa0d1941
a-sowa <axelsowapro@gmail.com>
2024-04-24 11:11:40 +0000

    Update description

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -7,5 +7,14 @@
 
 
 def add(a: float, b: float) -> float:
-    """Returns the sum of two floats."""
+    """
+        Add two float arguments
+
+        Args:
+            a (float): first float argument
+            b (float): second float argument
+
+        Returns:
+            float: sum of a and b
+    """
     return a + b
