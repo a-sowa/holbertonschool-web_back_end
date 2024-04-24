c3fa81b419631567c98f295e7d8ba7ebf0b337f2
a-sowa <axelsowapro@gmail.com>
2024-04-24 10:37:48 +0000

    Task 2 - Type-annotated function that returns the floor of the float

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+    Type-annotated function which takes
+    a float n as arguments
+    and returns the floor of the float.
+"""
+
+import math
+
+
+def floor(n: float) -> int:
+    """Returns the floor of the given float."""
+    return math.floor(n)
