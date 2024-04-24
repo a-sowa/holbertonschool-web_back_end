a82e12d00a9cbda2db3ba66719c78e42cd2e07de
a-sowa <axelsowapro@gmail.com>
2024-04-24 10:46:01 +0000

    Type-annotated function that returns the string representation of the float

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+    Type-annotated function that takes
+    a float n as arguments
+    and returns the string representation of the float.
+"""
+
+
+def to_str(n: float) -> str:
+    """Returns the string representation of the given float."""
+    return str(n)
