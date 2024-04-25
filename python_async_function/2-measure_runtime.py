75c94ea9a0df4e1b42d9bae4447c3a25ea5b8ad0
a-sowa <axelsowapro@gmail.com>
2024-04-25 07:59:34 +0000

    Fix

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -20,5 +20,4 @@ async def measure_time(n: int, max_delay: int) -> float:
     start_time = time.time()
     asyncio.run(wait_n(n, max_delay))
     end_time = time.time()
-    total_time = end_time - start_time
-    return total_time / n
+    return ((end_time - start_time) / n)
